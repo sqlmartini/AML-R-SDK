f91ac30ce4c5feeedb8ee363c2855fbb63177a99
Anthony Martin <sqlmartini@users.noreply.github.com>
2020-04-16 13:52:32 -0400

    Update 01_authentication.R

--- 01_authentication.R
+++ 01_authentication.R
@@ -1,9 +1,9 @@
 library(azuremlsdk)
 
 #Authenticate to AML workspace
-interactive_auth <- interactive_login_authentication(tenant_id = '72f988bf-86f1-41af-91ab-2d7cd011db47')
+interactive_auth <- interactive_login_authentication(tenant_id = '')
 
 ws <- get_workspace("azureml",
-                    "4b395f5d-d1ab-4bae-808d-a4398c9c61c5",
+                    "<subscription ID here>",
                     "azureml",
                     auth = interactive_auth)
